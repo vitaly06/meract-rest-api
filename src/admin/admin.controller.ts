ac8f8a626e563622cf0356190070322033729d51
Sadikov Vitaly <vitaly.sadikov1@yandex.ru>
2025-07-21 15:30:14 +0500

    fix: fixed update Admin, new dump.sql

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -6,12 +6,14 @@ import {
   Param,
   Post,
   Put,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { JwtAuthGuard } from 'src/common/guards/jwt.guard';
 import { CreateAdminRequest } from './dto/create-admin.dto';
 import { UpdateAdminRequest } from './dto/update-admin.dto';
+import { RequestWithUser } from 'src/auth/interfaces/request-with-user.dto';
 
 @Controller('admin')
 export class AdminController {
@@ -29,17 +31,24 @@ export class AdminController {
 
   @UseGuards(JwtAuthGuard)
   @Post('create-admin')
-  async createAdmin(@Body() dto: CreateAdminRequest) {
-    return await this.adminService.createAdmin(dto);
+  async createAdmin(
+    @Body() dto: CreateAdminRequest,
+    @Req() req: RequestWithUser,
+  ) {
+    return await this.adminService.createAdmin(dto, req);
   }
 
   @Put(':id')
-  async updateAdmin(@Body() dto: UpdateAdminRequest, @Param('id') id: string) {
-    return await this.adminService.update(dto, +id);
+  async updateAdmin(
+    @Body() dto: UpdateAdminRequest,
+    @Param('id') id: string,
+    @Req() req: RequestWithUser,
+  ) {
+    return await this.adminService.update(dto, +id, req);
   }
 
   @Delete(':id')
-  async deleteAdmin(@Param('id') id: string) {
-    return await this.adminService.delete(+id);
+  async deleteAdmin(@Param('id') id: string, @Req() req: RequestWithUser) {
+    return await this.adminService.delete(+id, req);
   }
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -2,15 +2,21 @@ import {
   BadRequestException,
   Injectable,
   NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateAdminRequest } from './dto/create-admin.dto';
 import * as bcrypt from 'bcrypt';
 import { UpdateAdminRequest } from './dto/update-admin.dto';
+import { UtilsService } from 'src/common/utils/utils.serivice';
+import { RequestWithUser } from 'src/auth/interfaces/request-with-user.dto';
 
 @Injectable()
 export class AdminService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly utilsService: UtilsService,
+  ) {}
 
   async findAll() {
     return await this.prisma.user.findMany({
@@ -48,7 +54,7 @@ export class AdminService {
     return checkAdmin;
   }
 
-  async createAdmin(dto: CreateAdminRequest) {
+  async createAdmin(dto: CreateAdminRequest, req: RequestWithUser) {
     const { login, password, email } = { ...dto };
     const role = await this.prisma.role.findUnique({
       where: { name: 'admin' },
@@ -66,7 +72,7 @@ export class AdminService {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    return await this.prisma.user.create({
+    const newAdmin = await this.prisma.user.create({
       data: {
         login,
         email,
@@ -74,34 +80,83 @@ export class AdminService {
         roleId: role.id,
       },
     });
+
+    // Current admin
+    const admin = await this.findById(req.user.sub);
+
+    await this.utilsService.addRecordToActivityJournal(
+      `The main administrator ${admin.login || admin.email} created the administrator ${newAdmin.login || newAdmin.email}`,
+      [admin.id, newAdmin.id],
+    );
   }
 
-  async update(dto: UpdateAdminRequest, id: number) {
+  async update(dto: UpdateAdminRequest, id: number, req: RequestWithUser) {
     const checkAdmin = await this.prisma.user.findUnique({
       where: { id },
     });
+
     if (!checkAdmin) {
       throw new NotFoundException('Admin with this id not found');
     }
 
-    dto.password = await bcrypt.hash(dto.password, 10);
+    const updateData: {
+      login?: string;
+      email?: string;
+      password?: string;
+    } = {};
 
-    await this.prisma.user.update({
-      where: { id },
-      data: {
-        ...dto,
-      },
-    });
+    if (dto.login && dto.login !== checkAdmin.login) {
+      updateData.login = dto.login;
+    }
 
-    return { message: 'admin updated successfully' };
+    if (dto.email && dto.email !== checkAdmin.email) {
+      updateData.email = dto.email;
+    }
+
+    if (dto.oldPassword && dto.newPassword) {
+      const comparePasswords = await bcrypt.compare(
+        dto.oldPassword,
+        checkAdmin.password,
+      );
+      if (!comparePasswords) {
+        throw new UnauthorizedException('Passwords do not match');
+      }
+      updateData.password = await bcrypt.hash(dto.newPassword, 10);
+    }
+
+    if (Object.keys(updateData).length > 0) {
+      await this.prisma.user.update({
+        where: { id },
+        data: updateData,
+      });
+
+      // Current admin
+      const admin = await this.findById(req.user.sub);
+
+      await this.utilsService.addRecordToActivityJournal(
+        `The main admin ${admin.login || admin.email} updated admin data for ${checkAdmin.login || checkAdmin.email}`,
+        [admin.id, checkAdmin.id],
+      );
+
+      return { message: 'Admin updated successfully' };
+    }
+
+    return { message: 'No changes detected' };
   }
 
-  async delete(id: number) {
-    await this.findById(id);
+  async delete(id: number, req: RequestWithUser) {
+    const checkAdmin = await this.findById(id);
 
     await this.prisma.user.delete({
       where: { id },
     });
+
+    const admin = await this.findById(req.user.sub);
+
+    await this.utilsService.addRecordToActivityJournal(
+      `The main administrator ${admin.login || admin.email} has deleted the administrator ${checkAdmin.login || checkAdmin.email}`,
+      [admin.id],
+    );
 
     return { message: 'Admin successfully removed' };
   }
