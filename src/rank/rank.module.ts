91103f6a5f5f6a154a85aaf17b836a0edb0b537e
unknown <vitaly.sadikov1@yandex.ru>
2025-11-26 20:17:47 +0500

    fix: fixed rank module

--- src/rank/rank.module.ts
+++ src/rank/rank.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RankService } from './rank.service';
 import { RankController } from './rank.controller';
+import { RankGateway } from './rank.gateway';
 
 @Module({
   controllers: [RankController],
-  providers: [RankService],
+  providers: [RankService, RankGateway],
 })
 export class RankModule {}
