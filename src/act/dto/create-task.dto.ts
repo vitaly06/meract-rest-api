c4d906a6d4a86a6cc0ce5216020a45c30f8370eb
unknown <vitaly.sadikov1@yandex.ru>
2025-11-30 20:26:54 +0500

    fix dto  for tasks

--- src/act/dto/create-task.dto.ts
+++ src/act/dto/create-task.dto.ts
@@ -1,6 +1,8 @@
 import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTaskDto {
+  @ApiProperty({ example: 'Reach Global Elite', description: 'Task title' })
   @IsNotEmpty()
   @IsString()
   @MaxLength(200)
