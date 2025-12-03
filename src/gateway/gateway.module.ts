bec8fa669f061af28f0d74bdd66f9b1b97a8e27e
unknown <vitaly.sadikov1@yandex.ru>
2025-12-03 14:19:51 +0500

    fixed ws

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -1,18 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { MainGateway } from './main.gateway';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 import { ChatModule } from '../chat/chat.module';
 import { GuildModule } from '../guild/guild.module';
 import { ActModule } from '../act/act.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule,
     JwtModule.register({}),
-    ChatModule,
-    GuildModule,
-    ActModule,
+    PrismaModule,
+    forwardRef(() => ChatModule),
+    forwardRef(() => GuildModule),
+    forwardRef(() => ActModule),
   ],
   providers: [MainGateway],
   exports: [MainGateway],
